8b1f22a02e42d915534a6c44464f650238a274b7
FurkanKzltprk <kiziltoprakfurkan001@gmail.com>
2025-03-13 16:57:18 +0300

    Arduino ile ilgili basit led yakma uygulaması

---
+++ Led_yak/Led_yak.ino
@@ -0,0 +1,30 @@
+
+/*#define yesil_led 8
+
+void setup() {
+ pinMode(yesil_led,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(yesil_led,HIGH);
+  delay(500);
+  digitalWrite(yesil_led,LOW);
+  delay(500);
+
+}*/
+
+# define sari_led 1
+
+void setup() {
+  pinMode(sari_led,OUTPUT);
+}
+void loop(){
+  digitalWrite(sari_led,HIGH);
+  delay(100);
+  digitalWrite(sari_led,LOW);
+  delay(100);
+}
+
+
+
---
+++ Led_yak/Readme.md
@@ -0,0 +1,2 @@
+Arduino ile ilgili led yakma uygulaması
+https://youtu.be/gKVO2i0EQt0
